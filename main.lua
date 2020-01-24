e229118195ad891f48a7e946ba240287afb19ce8
chenxuuu <lolicon@papapoi.com>
2020-01-24 15:39:09 +0800

    把必备的几个函数加上

--- main.lua
+++ main.lua
@@ -10,7 +10,7 @@ import("System.Text")
 --简化某些函数的语法
 CQLog = AppData.CQLog
 CQApi = AppData.CQApi
-CQLog:Debug("lua插件","加载虚拟机"..name)
+CQLog:Debug("lua插件","加载新虚拟机"..name)
 
 --重写print函数，重定向到debug接口输出
 function print(...)
@@ -25,16 +25,28 @@ function print(...)
 end
 
 --加上需要require的路径
-local rootPath = apiGetAsciiHex(apiGetPath())
+local rootPath = Utils.GetAsciiHex(CQApi.AppDirectory)
 rootPath = rootPath:gsub("[%s%p]", ""):upper()
 rootPath = rootPath:gsub("%x%x", function(c)
                                     return string.char(tonumber(c, 16))
                                 end)
 package.path = package.path..
-";"..rootPath.."data/app/com.papapoi.ReceiverMeow/lua/require/?.lua"
+";"..rootPath.."lua/require/?.lua"
 
 --加载字符串工具包
 require("strings")
+
+--重载几个可能影响中文目录的函数
+local oldrequire = require
+require = function (s)
+    local s = apiGetAsciiHex(s):fromHex()
+    return oldrequire(s)
+end
+local oldloadfile = loadfile
+loadfile = function (s)
+    local s = apiGetAsciiHex(s):fromHex()
+    return oldloadfile(s)
+end
 
 --安全的，带解析结果返回的json解析函数
 --返回值：数据,是否成功,错误信息
@@ -56,24 +68,49 @@ function jsonEncode(t)
     end
 end
 
-
-
-
-
---测试http
-local a = HttpWebClient.Get("https://qq.papapoi.com/delay?t=0",true)
-local t = {}
-for i=0,a.Length-1 do
-    table.insert(t,string.char(a[i]))
+--唯一id
+local idTemp = 0
+function getId()
+    idTemp = idTemp + 1--没必要判断是否溢出，溢出自动变成负数
+    return idTemp
 end
-print(table.concat(t))
 
-sys.async("Native.Csharp.Tool","Native.Csharp.Tool.Http.HttpWebClient.Get",
-{"https://qq.papapoi.com/delay?t=0",true},
-function (r,d)
-    print("cb",r,d)
-end)
+--封装一个异步的http get接口
+function asyncHttpGet(url,para,timeout,cookie)
+    local delayFlag = "http_get_"..os.time()..getId()--基本没有重复可能性的唯一标志
+    sys.async("com.papapoi.ReceiverMeow","Native.Csharp.App.LuaEnv.Utils.HttpGet",
+            {url or "",para or "",timeout or 5000,cookie or ""},
+    function (r,d)
+        sys.publish(delayFlag,r,d)
+    end)
+    return sys.waitUntil(delayFlag, timeout)
+end
 
+--封装一个异步的http post接口
+function asyncHttpPost(url,para,timeout,cookie,contentType)
+    local delayFlag = "http_post_"..os.time()..getId()--基本没有重复可能性的唯一标志
+    sys.async("com.papapoi.ReceiverMeow","Native.Csharp.App.LuaEnv.Utils.HttpPost",
+            {url or "",para or "",timeout or 5000,cookie or "",
+                contentType or "application/x-www-form-urlencoded"},
+    function (r,d)
+        sys.publish(delayFlag,r,d)
+    end)
+    return sys.waitUntil(delayFlag, timeout)
+end
+
+--加强随机数随机性
+math.randomseed(tostring(os.time()):reverse():sub(1, 6))
+
+--获取随机字符串
+function getRandomString(len)
+    local str = "1234567890abcdefhijklmnopqrstuvwxyz"
+    local ret = ""
+    for i = 1, len do
+        local rchr = math.random(1, string.len(str))
+        ret = ret .. string.sub(str, rchr, rchr)
+    end
+    return ret
+end
 
 sys.tiggerRegister("groupMessage",function (data)
     -- AppData.CQLog:Debug("Lua收到消息",data.msg)
