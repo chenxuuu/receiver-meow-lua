d6e380fd0ff1c525341491180fcb5f783c5f8943
chenxuuu <lolicon@papapoi.com>
2020-03-23 17:29:36 +0800

    增加bv号支持，依旧兼容av号

--- require/apps/13av.lua
+++ require/apps/13av.lua
@@ -1,19 +1,25 @@
-local function av(av)
-    av = av:match("av(%d+)")
-    local html = asyncHttpGet("http://api.bilibili.com/x/web-interface/view?aid="..av)
+local function av(bv)
+    local bvid = bv:match("bv(%w+)") or bv:match("BV(%w+)")
+    local html
+    if bvid then
+        html = asyncHttpGet("http://api.bilibili.com/x/web-interface/view?bvid="..bvid)
+    else
+        local av = bv:match("av(%d+)") or bv:match("AV(%d+)")
+        html = asyncHttpGet("http://api.bilibili.com/x/web-interface/view?aid="..av)
+    end
     if not html then return "查找失败" end
     local j,r,e = jsonDecode(html)
     if j.code ~= 0 then return "数据解析失败啦" end
     local image = j.data.pic and asyncImage(j.data.pic)
     return (image and image.."\r\n" or "")..
-    "av"..av..",标题："..j.data.title..
+    ",标题："..j.data.title..
     "\r\n"..j.data.desc:gsub("<br/>","\r\n")..
     "\r\n分区："..(j.data.tname and j.data.tname or "")
 end
 
 return {--b站av号解析
     check = function (data)
-        return data.msg:find("av%d+")
+        return data.msg:lower():find("av%d+") or data.msg:lower():find("bv%w+")
     end,
     run = function (data,sendMessage)
         sys.taskInit(function ()
