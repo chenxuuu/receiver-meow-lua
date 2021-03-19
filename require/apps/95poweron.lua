f5cffdb6079e37b22155dfc3c4abe4f2bfd8bd92
chenxuuu <lolicon@papapoi.com>
2021-03-19 22:22:00 +0800

    控制电脑开机

---
+++ require/apps/95poweron.lua
@@ -0,0 +1,13 @@
+return {--!addadmin
+check = function (data)
+    return data.msg == "开机" and data.qq == Utils.Setting.AdminQQ
+end,
+run = function (data,sendMessage)
+    if Mqtt.Publish(XmlApi.Get("settings","poweronTopic"), XmlApi.Get("settings","poweronPayload"), 0) then
+        sendMessage("已向你电脑发送开机指令")
+    else
+        sendMessage("我发不出去开机指令，你查查啥问题")
+    end
+    return true
+end,
+}
